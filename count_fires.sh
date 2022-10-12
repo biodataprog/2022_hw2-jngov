a0fa57b6becd6f2318437baefd76ecc33c16498b
Julie Ngov <114267873+jngov@users.noreply.github.com>
2022-10-12 11:29:24 -0700

    Update count_fires.sh

--- count_fires.sh
+++ count_fires.sh
@@ -9,16 +9,16 @@ curl -o calfire.csv https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::rece
 # cut -d, ....
 nano calfire.csv #deleted spacings between two lines
 cut -d, -f2 calfire.csv | sort | head -n 1
-cut -d, -f2 calfire.csv | sort | tail -n 2 #header
+cut -d, -f2 calfire.csv | tail -n +2 | sort | tail -n -1
 MINYEAR=$(cut -d, -f2 calfire.csv | sort | head -n 1)
-MAXTYEAR=$(cut -d, -f2 calfire.csv | tail -n +2| sort | tail -n 1)
+MAXYEAR=$(cut -d, -f2 calfire.csv | tail -n +2| sort | tail -n 1)
 # write code to set these variables with the smallest and largest years
 echo "This report has the years: $MINYEAR-$MAXYEAR"
 # if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
 
 # print out the total number of fires (remember to remove the header line)
 cut -d, -f2 calfire.csv | tail -n +2 | wc -l
-TOTALFILECOUNT=134
+TOTALFILECOUNT=TOTALFIRECOUNT=$(cut -d, -f2 calfire.csv | tail -n +2 | wc -l)
 # put your code here to update this variable
 echo "Total number of files: $TOTALFILECOUNT"
 
@@ -45,6 +45,27 @@ do
       echo "In Year $YEAR, Total was $TOTAL"
 done
 
+for YEAR in $(seq 2017 2021)
+do
+TOTAL=$(grep $YEAR calfires_2021.csv | awk -F',' '{sum+=$13;} END{print sum;}')
+ echo "In Year $YEAR, Total was $TOTAL"
+ done
+
+
+curl -o calfire.csv https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::recent-large-fire-perimeters-5000-acres.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D
+nano calfire.csv #deleted spacings between two lines
+
+cut -d, -f2 calfire.csv | sort | head -n 1
+cut -d, -f2 calfire.csv | tail -n +2 | sort | tail -n -1
+MINYEAR=$(cut -d, -f2 calfire.csv | sort | head -n 1)
+MAXYEAR=$(cut -d, -f2 calfire.csv | tail -n +2| sort | tail -n 1)
+echo "This report has the years: $MINYEAR-$MAXYEAR"
+TOTALFILECOUNT=TOTALFIRECOUNT=$(cut -d, -f2 calfire.csv | tail -n +2 | wc -l)
+echo "Number of fires in each year follows: $(cut -d, -f2 calfire.csv | tail -n +2 | sort | uniq -c)"
+awk -F, '{print $13,$6}' calfire.csv | sort -n | tail -n 1
+LARGEST="August Complex"
+LARGESTACRES="1032699.6"
+echo "Largest fire was $LARGEST and burned $LARGESTACRES"
 for YEAR in $(seq 2017 2021)
 do
 TOTAL=$(grep $YEAR calfires_2021.csv | awk -F',' '{sum+=$13;} END{print sum;}')
