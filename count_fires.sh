5ad67967f664d6a8b32306035bc4ca03326f5399
Julie Ngov <114267873+jngov@users.noreply.github.com>
2022-10-12 09:20:13 -0700

    Update count_fires.sh

--- count_fires.sh
+++ count_fires.sh
@@ -10,23 +10,32 @@ curl -o calfire.csv https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::rece
 nano calfire.csv #deleted spacings between two lines
 cut -d, -f2 calfire.csv | sort | head -n 1
 cut -d, -f2 calfire.csv | sort | tail -n 2 #header
-MINYEAR=2017
-MAXTYEAR=2021
+MINYEAR=$(cut -d, -f2 calfire.csv | sort | head -n 1)
+MAXTYEAR=$(cut -d, -f2 calfire.csv | tail -n +2| sort | tail -n 1)
 # write code to set these variables with the smallest and largest years
 echo "This report has the years: $MINYEAR-$MAXYEAR"
 # if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
 
 # print out the total number of fires (remember to remove the header line)
-TOTALFILECOUNT=0
+cut -d, -f2 calfire.csv | tail -n +2 | wc -l
+TOTALFILECOUNT=134
 # put your code here to update this variable
 echo "Total number of files: $TOTALFILECOUNT"
 
 # print out the number of fire in each year
+cut -d, -f2 calfire.csv | tail -n +2 | uniq -c
 echo "Number of fires in each year follows:"
+echo "Number of fires in each year follows: $(cut -d, -f2 calfire.csv | tail -n +2 | uniq -c)"
 
-
+awk -F, '{print $13,$6}' calfire.csv | sort -n | tail -n 1
+LARGEST="August Complex"
+LARGESTACRES="1032699.6"
 # print out the name of the largest file use the GIS_ACRES and report the number of acres
 echo "Largest fire was $LARGEST and burned $LARGESTACRES"
+F13 acre
+F6 name
+
+
 
 # print out the years - change the code in $(echo 1990) to print out the years (hint - how did you get MINYEAR and MAXYEAR?
 for YEAR in $(echo 1990)
